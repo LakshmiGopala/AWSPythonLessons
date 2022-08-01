9b283ae6aeb1f1456fcb1db5a1d0e920250327ee
LakshmiGopala <110236297+LakshmiGopala@users.noreply.github.com>
2022-08-01 16:45:22 +0530

    Add files via upload

---
+++ str1.py
@@ -0,0 +1,39 @@
+'''n1=int(input("Enter Any  number"))
+n2=float(input("Enter Another  number"))
+print(n1+n2)
+print(type(n1))
+print(type(n2))'''
+'''para1='Anudip is a skill development oraganization'
+if 'skillo' not in para1:
+    print("Yes Not Present")
+else:
+    print("Yes Present")
+#print(para1[::-1])
+'''
+'''
+path=R"C:\t\n\e\b\dass"
+print(path)
+'''
+#name="Sachin"
+#age=25
+#sal=1500
+#print("Hello ",name," Your Age is  ",str(age) ," Your Salary is ",str(sal))
+#print("Hello %s Your Age is %i  Your Salary is %d " % (name,age,sal))
+#print("Hello {n} Your Age is {a}  Your Salary is {s} ".format(n=name,a=age,s=sal))
+#para1='anudip'
+#print(para1.capitalize())
+#print(para1.upper())
+#para2=para1.upper();
+#print(para1 if para1.isupper() else para1.upper())
+#print(para1.casefold())
+#mob="9851613685"
+#print(mob.center(14,"X"))
+#name="DipDAS"
+#print(name)
+#print(name.isdigit())
+#print(name.strip())
+#print(name.swapcase())
+country="India,Nepal,Bhutan"
+data=country.split(",")
+for x in data:
+    print(x)
---
+++ tup.py
@@ -0,0 +1,5 @@
+t=(20,26,587,65,52,32)
+print(t)
+print(t[0])
+for i in t:
+    print(i)
